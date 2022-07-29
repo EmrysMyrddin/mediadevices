6b33fae64143f4869f4324b27011b97b6219b9e1
Valentin Cocaud <v.cocaud@gmail.com>
2022-07-29 16:21:21 +0200

    ignore up to 3 decode errors in a row

--- pkg/frame/frame.go
+++ pkg/frame/frame.go
@@ -1,6 +1,10 @@
 package frame
 
-import "image"
+import (
+	"errors"
+	"fmt"
+	"image"
+)
 
 type Decoder interface {
 	Decode(frame []byte, width, height int) (image.Image, func(), error)
@@ -10,5 +14,35 @@ type Decoder interface {
 type decoderFunc func(frame []byte, width, height int) (image.Image, func(), error)
 
 func (f decoderFunc) Decode(frame []byte, width, height int) (image.Image, func(), error) {
-	return f(frame, width, height)
+	img, release, err := f(frame, width, height)
+	if err != nil {
+		err = &decoderError{cause: err}
+	}
+	return img, release, err
+}
+
+var DecoderError = errors.New("decoder error")
+
+type decoderError struct {
+	cause error
+}
+
+func (err *decoderError) Error() string {
+	return fmt.Sprintf("%s: %s", DecoderError, err.cause)
+}
+
+func (err *decoderError) Cause() error {
+	return err.cause
+}
+
+func (err *decoderError) Unwrap() error {
+	return err.cause
+}
+
+func (err *decoderError) Is(target error) bool {
+	return errors.Is(target, DecoderError)
+}
+
+func (err *decoderError) String() string {
+	return err.Error()
 }
